a02372b08b3f1e9d24217d2b060627ac8d5befdb
freeman lo <flo@radford.edu>
2023-05-04 22:22:11 -0400

    added uninstall and update script for python311

---
+++ python3/python311/3.11.3/uninstall_python311.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Auth   : Freeman
+# Email  : flo@radford.edu
+# DESC   : Silent delete Tool for MAC_OSX Python
+#        : 
+# Date   : 2023.05.04
+################################
+
+# Uninstall Python 3.11 for Mac
+echo "Uninstalling Python 3.11.x for Mac..."
+
+cd
+rm -fr ./Library/Python
+echo "Required Administrator Password for the following. "
+sudo rm -fr /Library/Frameworks/Python.framework/
+sudo rm -fr /Applications/Python\ 3.11/
+
+echo "Finished python Uninstallation process."
---
+++ python3/python311/3.11.3/update_python311.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Auth   : Freeman
+# Email  : flo@radford.edu
+# DESC   : Silent updating Tool for MAC_OSX Python
+#        : 
+# Date   : 2023.05.04
+################################
+
+# Uninstall Python 3.11 for Mac
+echo "Updating Python 3.11.x for Mac..."
+
+cd
+# updating pip
+/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install --upgrade pip
+# write out the python packages to a file then upgrade them individually
+/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 freeze > req.txt
+/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install -r req.txt --upgrade
+
+# delete the req file
+rm -f req.txt
+
+echo "Finished Updating python Tools."
