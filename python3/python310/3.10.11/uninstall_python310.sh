b2523f54f10036775a53bf53d463871dd6d574bd
freeman lo <flo@radford.edu>
2023-05-04 22:18:13 -0400

    added an update and uninstall python310 script

---
+++ python3/python310/3.10.11/uninstall_python310.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Auth   : Freeman
+# Email  : flo@radford.edu
+# DESC   : Silent delete Tool for MAC_OSX Python
+#        : 
+# Date   : 2023.05.04
+################################
+
+# Uninstall Python 3.10 for Mac
+echo "Uninstalling Python 3.x.x for Mac..."
+
+rm -fr ./Library/Python
+echo "Required Administrator Password for the following. "
+sudo rm -fr /Library/Frameworks/Python.framework/
+sudo rm -fr /Applications/Python\ 3.10/
+
+echo "Finished python Uninstallation process."
