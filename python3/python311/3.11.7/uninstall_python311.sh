e00ad39d4ff7152c3f048e5c85bb104aa663df17
freemanbach <flo@radford.edu>
2024-02-21 13:57:16 -0500

    removed the dot after echo

--- python3/python311/3.11.7/uninstall_python311.sh
+++ python3/python311/3.11.7/uninstall_python311.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/env bash
+#!/usr/bin/env bash
 # -----------------------------------------------------------------------------------
 # Auth   : Freeman
 # Email  : flo@radford.edu
@@ -8,12 +8,12 @@
 # -----------------------------------------------------------------------------------
 
 # Uninstall Python 3.11 for Mac
-echo "Uninstalling Python 3.11.x for Mac..."
+echo "Uninstalling Python 3.11.x for Mac... \n"
 
 cd
 rm -fr ./Library/Python
-echo "Required Administrator Password for the following. "
+echo "Required Administrator Password for the following. \n"
 sudo rm -fr /Library/Frameworks/Python.framework/
 sudo rm -fr /Applications/Python\ 3.11/
 
-echo "Finished python Uninstallation process."
+echo "Finished python Uninstallation process.\n\n"
--- python3/python311/3.11.7/update_python311.sh
+++ python3/python311/3.11.7/update_python311.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/env bash
+#!/usr/bin/env bash
 # -----------------------------------------------------------------------------------
 # Auth   : Freeman
 # Email  : flo@radford.edu
@@ -9,23 +9,26 @@
 
 # Update Python 3.11 on Mac
 echo "Updating Python 3.11.x for Mac..."
+echo "\n"
 
 # send user back to $HOME first
 cd
 
+echo "Required Administrator Password for the following updates. "
+echo "\n"
 DATA=/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
 if [[ -f "$DATA" ]]; then
     echo "Starting to update to new version of pip.\n "
-    /Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install --upgrade pip
+    sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install --upgrade pip
     # write out the python packages to a file then upgrade them individually
-    /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 freeze > req.txt
-    /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install -r req.txt --upgrade
+    sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 freeze > req.txt
+    sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install -r req.txt --upgrade
     # Delete req.txt file
     echo "deleting the req file. \n"
-    rm -f req.txt
+    sudo rm -f req.txt
 else
     echo "python3 file was not found in the data location.\n"
     exit 1
 fi
 
-echo "Finished Updating python Tools. \n"
+echo "Finished Updating python Tools. \n\n"
