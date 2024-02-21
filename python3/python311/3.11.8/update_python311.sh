d80af139eb89a5a2b86a7794e733e2aa14b48ec2
freeman <aschenbach@gmail.com>
2024-02-21 10:43:06 -0500

    removed the periods

--- python3/python311/3.11.8/update_python311.sh
+++ python3/python311/3.11.8/update_python311.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/env bash
+#!/usr/bin/env bash
 # -----------------------------------------------------------------------------------
 # Auth   : Freeman
 # Email  : flo@radford.edu
@@ -9,13 +9,13 @@
 
 # Update Python 3.11 on Mac
 echo "Updating Python 3.11.x for Mac..."
-echo.
+echo "\n"
 
 # send user back to $HOME first
 cd
 
 echo "Required Administrator Password for the following updates. "
-echo.
+echo "\n"
 DATA=/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
 if [[ -f "$DATA" ]]; then
     echo "Starting to update to new version of pip.\n "
