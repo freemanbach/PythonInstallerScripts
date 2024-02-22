9570c81ad5644a997cd08bc3c0fa51454b6ad4bf
freemanbach <flo@radford.edu>
2024-02-21 21:58:38 -0500

    added sudo on pip

--- python3/python311/3.11.6/update_python311.sh
+++ python3/python311/3.11.6/update_python311.sh
@@ -1,31 +1,34 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
+# -----------------------------------------------------------------------------------
 # Auth   : Freeman
 # Email  : flo@radford.edu
 # DESC   : Silent updating Tool for MAC_OSX Python
 #        : 
 # Date   : 2023.10.02
-################################
+# -----------------------------------------------------------------------------------
 
-# Uninstall Python 3.11 for Mac
+# Update Python 3.11 on Mac
 echo "Updating Python 3.11.x for Mac..."
+echo "\n"
 
 # send user back to $HOME first
 cd
 
+echo "Required Administrator Password for the following updates. "
+echo "\n"
 DATA=/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
 if [[ -f "$DATA" ]]; then
     echo "Starting to update to new version of pip.\n "
-    /Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install --upgrade pip
+    sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install --upgrade pip
     # write out the python packages to a file then upgrade them individually
-    /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 freeze > req.txt
-    /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install -r req.txt --upgrade
+    sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 freeze > req.txt
+    sudo /Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install -r req.txt --upgrade
     # Delete req.txt file
     echo "deleting the req file. \n"
-    rm -f req.txt
+    sudo rm -f req.txt
 else
     echo "python3 file was not found in the data location.\n"
     exit 1
 fi
 
-echo "Finished Updating python Tools. \n"
+echo "Finished Updating python Tools. \n\n"
